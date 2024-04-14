0f677654f69a5ee60a7f76de79fab10e19991b12
Anuska Ghosh <157162260+anuska23@users.noreply.github.com>
2024-04-14 19:06:30 +0530

    script.js

--- js/script.js
+++ js/script.js
@@ -7,12 +7,11 @@ let btn = document.getElementById('btn');
 
 incomeForm.addEventListener('submit', function(event) {
     event.preventDefault();
-    // Get numerical values from input elements
+  
     let grossIncomeValue = parseFloat(grossIncome.value);
     let extraIncomeValue = parseFloat(extraIncome.value);
     let deductionValue = parseFloat(deduction.value);
 
-    // Calculate overall income after deductions
     let overallIncome = grossIncomeValue + extraIncomeValue - deductionValue;
 
     let tax = 0;
@@ -37,28 +36,23 @@ incomeForm.addEventListener('submit', function(event) {
         tax = taxrate * (overallIncome - 800000);
     }
     
-    // Display result
     let taxResultElement = document.getElementById("taxResult");
     taxResultElement.innerHTML = "Your overall income will be" + "<br><center>" + tax.toFixed(2) + "<br>"+"<span style='font-size: smaller;'> after tax deduction</span>";
 
     var modal = document.getElementById('myModal');
     modal.style.display = "block";
     
-    // Clear form fields
     grossIncome.value = '';
     extraIncome.value = '';
     ageGroup.value = '';
     deduction.value = '';
     
-    //Get the <span> element that closes the modal
     var close = document.getElementsByClassName("close")[0];
     
-    // When the user clicks on the close button, close the modal
     close.onclick = function() {
         modal.style.display = "none";
     };
 
-    // When the user clicks anywhere outside of the modal, close it
     window.onclick = function(event) {
         if (event.target == modal) {
             modal.style.display = "none";
